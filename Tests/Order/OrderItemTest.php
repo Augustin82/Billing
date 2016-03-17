866d198235b9a6c6c196ca4b6ab371ad6d2e05f1
Nicolas Macherey <nicolas.macherey@gmail.com>
2016-03-17 21:41:58 +0100

    [+][ADD][Billing] Refactoring to PSR-4

--- Tests/Order/OrderItemTest.php
+++ Tests/Order/OrderItemTest.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace UCS\Component\Billing\Tests\Order;
+namespace Tests\UCS\Component\Billing\Tests\Order;
 
 /* imports */
 use UCS\Component\Billing\Order\OrderItem;
